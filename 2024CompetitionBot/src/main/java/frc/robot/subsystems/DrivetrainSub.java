d8e949b8ea603b3ddd902f15753cedb30de91bef
4917 Roomba <software+roomba@4917.ca>
2024-01-19 16:25:04 -0500

    Added DrivetrainSub

---
+++ 2024CompetitionBot/src/main/java/frc/robot/subsystems/DrivetrainSub.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.kauailabs.navx.frc.AHRS;
+
+public class DrivetrainSub extends SubsystemBase {
+  private static final boolean kGyroReversed = false;
+
+  private final Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.318);
+  private final Translation2d m_frontRightLocation = new Translation2d(0.381, -0.318);
+  private final Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.318);
+  private final Translation2d m_backRightLocation = new Translation2d(-0.381, -0.318);
+
+  private final SwerveModule m_frontLeft = new SwerveModule(1, 2);
+  private final SwerveModule m_frontRight = new SwerveModule(3, 4);
+  private final SwerveModule m_backLeft = new SwerveModule(5, 6);
+  private final SwerveModule m_backRight = new SwerveModule(7, 8);
+
+  private final AHRS m_gyro = new AHRS(SPI.Port.kMXP);
+
+  private final SwerveDriveKinematics m_kinematics =
+      new SwerveDriveKinematics(m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+
+  /** Creates a new DrivetrainSub. */
+  public DrivetrainSub() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+
+}
