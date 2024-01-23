5e3c77a02f8394bf12b08c5ef6a9e93956b0edc8
4917 Roomba <software+roomba@4917.ca>
2024-01-22 19:50:14 -0500

    Update ShooterFlywheelCmd.java
    Added notes for possible command set up

--- 2024CompetitionBot/src/main/java/frc/robot/commands/ShooterFlywheelCmd.java
+++ 2024CompetitionBot/src/main/java/frc/robot/commands/ShooterFlywheelCmd.java
@@ -38,8 +38,20 @@ public class ShooterFlywheelCmd extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    /*
+     * Step 1: Set target velocity for flywheel speed + inverted or forward
+     * Step 2: Start flywheel
+     * note - Step 1 and 2 might need to be swapped
+     * Step 3: check if note is there (to see if note has left the storage) if it has go step 4, else continue
+     * Step 4: Stop fly wheel
+     * 
+     * 
+     */
+
+
     double driveOutput = m_FlyWheelPID.calculate(m_ShooterSub.getFlywheelVelocity(), 10); //10 is a target velocity we don't know what it is
     m_ShooterSub.spinFlywheel(driveOutput);
+
   }
 
   // Called once the command ends or is interrupted.
