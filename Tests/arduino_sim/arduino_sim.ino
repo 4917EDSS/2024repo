3503abcc368d628d7f520fdd5aedafe198743e5d
4917 Claptrap <software@4917.ca>
2024-02-10 12:57:05 -0500

    arduino code

--- Tests/arduino_sim/arduino_sim.ino
+++ Tests/arduino_sim/arduino_sim.ino
@@ -1,4 +1,4 @@
-unsigned char packetData[7] = {0};
+unsigned char packetData[8] = {0};
 unsigned int sensors[2] = {0,100};
 int dataLength = 4;
 
@@ -6,9 +6,11 @@ int dataLength = 4;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  packetData[0] = 0xA5;
-  packetData[1] = dataLength + 1;
+  Serial.begin(38400);
+  //Serial.begin(9600);
+  packetData[0] = 0xFF;
+  packetData[1] = 0xA5;
+  packetData[2] = dataLength + 1;
 
   pinMode(BUTTON_PIN, INPUT);
 }
@@ -19,13 +21,13 @@ void loop() {
   unsigned char checksum = 0;
   int index = 0;
 
-  packetData[2] = sensors[0] & 0xFF;
-  packetData[3] = (sensors[0] >> 8) & 0xFF;
-  packetData[4] = sensors[1] & 0xFF;
-  packetData[5] = (sensors[1] >> 8) & 0xFF;
+  packetData[3] = sensors[0] & 0xFF;
+  packetData[4] = (sensors[0] >> 8) & 0xFF;
+  packetData[5] = sensors[1] & 0xFF;
+  packetData[6] = (sensors[1] >> 8) & 0xFF;
 
   // Checksum the header, length and data bytes but not the checksum byte
-  for(index = 0; index < dataLength + 2; index++) {
+  for(index = 0; index < dataLength + 3; index++) {
     checksum += packetData[index];
   }
   packetData[6] = checksum;
@@ -34,9 +36,13 @@ void loop() {
   sensors[0]++;
   sensors[1]++;
 
-  unsigned char myBite = 0xA5;
+  //unsigned char myBite = 0xA5;
+  
   //while(digitalRead(BUTTON_PIN) == HIGH);
-//  Serial.write(packetData, dataLength + 3);
-Serial.write(&myBite, 1);
+   Serial.write(packetData, dataLength + 4);
+  //Serial.write(&packetData[0],1); //1010110101
+  //Serial.write(&packetData[1],1); //1010110101
+  //Serial.write(&packetData[2],1); //1010110101
+  //delay(1); //would like 260 micro sec  0.000260
   //while(digitalRead(BUTTON_PIN) == LOW);
 }
