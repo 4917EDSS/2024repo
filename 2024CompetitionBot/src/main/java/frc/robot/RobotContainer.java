d0899e950ce4ac0e1603b981e4eba90e546646a9
4917-Stronghold <software@4917.ca>
2024-02-12 19:02:38 -0500

    Added button for Upper and Lower FeedRolls and Flywheel

--- 2024CompetitionBot/src/main/java/frc/robot/RobotContainer.java
+++ 2024CompetitionBot/src/main/java/frc/robot/RobotContainer.java
@@ -11,11 +11,13 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.Constants.Shooter;
 import frc.robot.commands.ClimbCmdSetHeightCmd;
 import frc.robot.commands.DrivePathCmd;
 import frc.robot.commands.DriveToRelativePositionCmd;
@@ -37,179 +39,193 @@ import frc.robot.subsystems.VisionSub;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final LedSub m_ledSub = new LedSub();
-  private final ClimbSub m_climbSub = new ClimbSub();
-  private final DrivetrainSub m_drivetrainSub = new DrivetrainSub();
-  private final IntakeSub m_intakeSub = new IntakeSub();
-  private final ShooterSub m_shooterSub = new ShooterSub(m_ledSub);
-  private final VisionSub m_visionSub = new VisionSub();
+        // The robot's subsystems and commands are defined here...
+        private final LedSub m_ledSub = new LedSub();
+        private final ClimbSub m_climbSub = new ClimbSub();
+        private final DrivetrainSub m_drivetrainSub = new DrivetrainSub();
+        private final IntakeSub m_intakeSub = new IntakeSub();
+        private final ShooterSub m_shooterSub = new ShooterSub(m_ledSub);
+        private final VisionSub m_visionSub = new VisionSub();
 
-  private boolean m_isRedAlliance = true;
+        private boolean m_isRedAlliance = true;
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandPS4Controller m_driverController =
-      new CommandPS4Controller(OperatorConstants.kDriverControllerPort);
-  private final CommandPS4Controller m_operatorController =
-      new CommandPS4Controller(OperatorConstants.kOperatorControllerPort);
+        // Replace with CommandPS4Controller or CommandJoystick if needed
+        private final CommandPS4Controller m_driverController =
+                        new CommandPS4Controller(OperatorConstants.kDriverControllerPort);
+        private final CommandPS4Controller m_operatorController =
+                        new CommandPS4Controller(OperatorConstants.kOperatorControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    m_drivetrainSub.setDefaultCommand(
-        new DriverFieldRelativeDriveCmd(m_drivetrainSub, m_driverController));
+        /** The container for the robot. Contains subsystems, OI devices, and commands. */
+        public RobotContainer() {
+                m_drivetrainSub.setDefaultCommand(
+                                new DriverFieldRelativeDriveCmd(m_drivetrainSub, m_driverController));
 
-    // Configure the button bindings
-    configureBindings();
-  }
+                // Configure the button bindings
+                configureBindings();
+        }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Driver controller bindings
-    m_driverController.L3()
-        .onTrue(new KillAllCmd(m_climbSub, m_drivetrainSub, m_intakeSub, m_shooterSub));
-    m_driverController.R3()
-        .onTrue(new KillAllCmd(m_climbSub, m_drivetrainSub, m_intakeSub, m_shooterSub));
+        /**
+         * Use this method to define your trigger->command mappings. Triggers can be created via the
+         * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+         * predicate, or via the named factories in {@link
+         * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+         * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+         * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+         * joysticks}.
+         */
+        private void configureBindings() {
+                // Driver controller bindings
+                m_driverController.L3()
+                                .onTrue(new KillAllCmd(m_climbSub, m_drivetrainSub, m_intakeSub, m_shooterSub));
+                m_driverController.R3()
+                                .onTrue(new KillAllCmd(m_climbSub, m_drivetrainSub, m_intakeSub, m_shooterSub));
 
-    // m_driverController.square() FREE
+                // m_driverController.square() FREE
 
-    m_driverController.cross()
-        .onTrue(new ClimbCmdSetHeightCmd(Constants.Climb.kHeightHookLowered, 0.5,
-            m_drivetrainSub,
-            m_climbSub));
+                m_driverController.cross()
+                                .onTrue(new ClimbCmdSetHeightCmd(Constants.Climb.kHeightHookLowered, 0.5,
+                                                m_drivetrainSub,
+                                                m_climbSub));
 
-    m_driverController.circle()
-        .onTrue(new ClimbCmdSetHeightCmd(Constants.Climb.kHeightTallHookRaised, 0.5,
-            m_drivetrainSub,
-            m_climbSub));
+                m_driverController.circle()
+                                .onTrue(new ClimbCmdSetHeightCmd(Constants.Climb.kHeightTallHookRaised, 0.5,
+                                                m_drivetrainSub,
+                                                m_climbSub));
 
-    m_driverController.triangle()
-        .onTrue(new ClimbCmdSetHeightCmd(Constants.Climb.kHeightShortHookRaised, 0.5,
-            m_drivetrainSub,
-            m_climbSub));
+                m_driverController.triangle()
+                                .onTrue(new ClimbCmdSetHeightCmd(Constants.Climb.kHeightShortHookRaised, 0.5,
+                                                m_drivetrainSub,
+                                                m_climbSub));
 
-    m_driverController.L1()
-        .whileTrue(
-            new StartEndCommand(() -> m_climbSub.setClimbPowerLeft(1.0),
-                () -> m_climbSub.setClimbPowerLeft(0.0)));
+                m_driverController.L1()
+                                .whileTrue(
+                                                new StartEndCommand(() -> m_climbSub.setClimbPowerLeft(1.0),
+                                                                () -> m_climbSub.setClimbPowerLeft(0.0)));
 
-    m_driverController.R1()
-        .whileTrue(
-            new StartEndCommand(() -> m_climbSub.setClimbPowerRight(1.0),
-                () -> m_climbSub.setClimbPowerRight(0.0)));
+                m_driverController.R1()
+                                .whileTrue(
+                                                new StartEndCommand(() -> m_climbSub.setClimbPowerRight(1.0),
+                                                                () -> m_climbSub.setClimbPowerRight(0.0)));
 
-    m_driverController.L2()
-        .whileTrue(
-            new StartEndCommand(() -> m_climbSub.setClimbPowerLeft(-1.0),
-                () -> m_climbSub.setClimbPowerLeft(0.0)));
+                m_driverController.L2()
+                                .whileTrue(
+                                                new StartEndCommand(() -> m_climbSub.setClimbPowerLeft(-1.0),
+                                                                () -> m_climbSub.setClimbPowerLeft(0.0)));
 
-    m_driverController.R2()
-        .whileTrue(
-            new StartEndCommand(() -> m_climbSub.setClimbPowerRight(-1.0),
-                () -> m_climbSub.setClimbPowerRight(0.0)));
+                m_driverController.R2()
+                                .whileTrue(
+                                                new StartEndCommand(() -> m_climbSub.setClimbPowerRight(-1.0),
+                                                                () -> m_climbSub.setClimbPowerRight(0.0)));
 
-    m_driverController.share()
-        .onTrue(new InstantCommand(() -> m_drivetrainSub.resetGyro(), m_drivetrainSub));
+                m_driverController.share()
+                                .onTrue(new InstantCommand(() -> m_drivetrainSub.resetGyro(), m_drivetrainSub));
 
-    m_driverController.options().onTrue(new TestLedsCmd(m_ledSub, LedColour.YELLOW));
+                m_driverController.options().onTrue(new TestLedsCmd(m_ledSub, LedColour.YELLOW));
 
-    m_driverController.touchpad().onTrue(new TestLedsCmd(m_ledSub, LedColour.BLUE));
+                m_driverController.touchpad().onTrue(new TestLedsCmd(m_ledSub, LedColour.BLUE));
 
-    m_driverController.PS().onTrue(new InstantCommand(() -> m_drivetrainSub.fun(), m_drivetrainSub));
+                m_driverController.PS().onTrue(new InstantCommand(() -> m_drivetrainSub.fun(), m_drivetrainSub));
 
-    // m_driverController.povRight()
-    //                 .onTrue(new DriveToRelativePositionCmd(m_drivetrainSub,
-    //                                 new Pose2d(2.0, 0.0, Rotation2d.fromDegrees(90.0))));
-    m_driverController.povRight().onTrue(new DrivePathCmd(m_drivetrainSub));
+                // m_driverController.povRight()
+                //                 .onTrue(new DriveToRelativePositionCmd(m_drivetrainSub,
+                //                                 new Pose2d(2.0, 0.0, Rotation2d.fromDegrees(90.0))));
+                m_driverController.povRight().onTrue(new DrivePathCmd(m_drivetrainSub));
 
-    m_driverController.povDown()
-        .onTrue(new DriveToRelativePositionCmd(m_drivetrainSub,
-            new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(-90.0))));
+                m_driverController.povDown()
+                                .onTrue(new DriveToRelativePositionCmd(m_drivetrainSub,
+                                                new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(-90.0))));
 
-    m_driverController.povLeft()
-        .onTrue(new DriveToRelativePositionCmd(m_drivetrainSub,
-            new Pose2d(-2.0, 0.0, Rotation2d.fromDegrees(-90.0))));
+                m_driverController.povLeft()
+                                .onTrue(new DriveToRelativePositionCmd(m_drivetrainSub,
+                                                new Pose2d(-2.0, 0.0, Rotation2d.fromDegrees(-90.0))));
 
-    m_driverController.povUp()
-        .onTrue(new DriveToRelativePositionCmd(m_drivetrainSub,
-            new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(90.0))));
+                m_driverController.povUp()
+                                .onTrue(new DriveToRelativePositionCmd(m_drivetrainSub,
+                                                new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(90.0))));
 
 
-    // Operator controller bindings
-    m_operatorController.L3()
-        .onTrue(new KillAllCmd(m_climbSub, m_drivetrainSub, m_intakeSub, m_shooterSub));
-    m_operatorController.R3()
-        .onTrue(new KillAllCmd(m_climbSub, m_drivetrainSub, m_intakeSub, m_shooterSub));
+                // Operator controller bindings
+                m_operatorController.L3()
+                                .onTrue(new KillAllCmd(m_climbSub, m_drivetrainSub, m_intakeSub, m_shooterSub));
+                m_operatorController.R3()
+                                .onTrue(new KillAllCmd(m_climbSub, m_drivetrainSub, m_intakeSub, m_shooterSub));
 
-    // m_operatorController.square()
+                m_operatorController.square().whileTrue(
+                                new StartEndCommand(() -> m_shooterSub.spinUpperFeeder(0.25),
+                                                () -> m_shooterSub.spinUpperFeeder(0.0)));
 
-    // m_operatorController.cross()
+                m_operatorController.cross().whileTrue(
+                                new StartEndCommand(() -> m_shooterSub.spinLowerFeeder(0.50),
+                                                () -> m_shooterSub.spinLowerFeeder(0.0)));
 
-    // m_operatorController.circle()
+                m_operatorController.circle().whileTrue(
+                                new StartEndCommand(() -> m_shooterSub.spinLowerFeeder(-0.50),
+                                                () -> m_shooterSub.spinLowerFeeder(0.0)));
 
-    // m_operatorController.triangle()
+                m_operatorController.triangle().whileTrue(
+                                new StartEndCommand(() -> m_shooterSub.spinUpperFeeder(-0.25),
+                                                () -> m_shooterSub.spinUpperFeeder(0.0)));
 
-    // m_operatorController.L1()
+                // m_operatorController.L1()
 
-    // m_operatorController.R1()
+                // m_operatorController.R1()
 
-    // m_operatorController.L2()
+                // m_operatorController.L2()
 
-    // m_operatorController.R2()
+                // m_operatorController.R2()
 
-    // m_operatorController.share()
+                // m_operatorController.share()
 
-    // m_operatorController.options()
+                // m_operatorController.options()
 
-    // m_operatorController.PS()
+                // m_operatorController.PS()
 
-    // m_operatorController.touchpad()
+                // m_operatorController.touchpad()
 
-    // m_operatorController.povUp()
+                // m_operatorController.povUp()
 
-    // m_operatorController.povRight()
+                // m_operatorController.povRight()
 
-    // m_operatorController.povDown()
+                // m_operatorController.povDown()
 
-    // m_operatorController.povLeft()
-  }
+                // m_operatorController.povLeft()
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return new PrintCommand("No auto yet");
-  }
+                m_shooterSub.setDefaultCommand(new RunCommand(
+                                () -> m_shooterSub.spinFlywheel(m_operatorController.getRightY()),
+                                m_shooterSub));
 
-  // intialize the sub systems
-  // TODO couple initialize need to be done
-  public void initSubsystems() {
-    // int sensorArray[] = new int[2];
-    // sensorArray = m_shooterSub.RS232Listen(); ////////////////////////TODO Remove
-    m_ledSub.init();
-    m_climbSub.init();
-    m_drivetrainSub.init();
-    m_intakeSub.init();
-    m_shooterSub.init();
-    m_visionSub.init();
+                // m_operatorController.getRightX
+        }
 
-    if(DriverStation.getAlliance().isPresent()) {
-      if(DriverStation.getAlliance().get() == Alliance.Red) {
-        m_isRedAlliance = true;
-      } else if(DriverStation.getAlliance().get() == Alliance.Blue) {
-        m_isRedAlliance = false;
-      }
-    }
-  }
+        /**
+         * Use this to pass the autonomous command to the main {@link Robot} class.
+         *
+         * @return the command to run in autonomous
+         */
+        public Command getAutonomousCommand() {
+                // An example command will be run in autonomous
+                return new PrintCommand("No auto yet");
+        }
+
+        // intialize the sub systems
+        // TODO couple initialize need to be done
+        public void initSubsystems() {
+                // int sensorArray[] = new int[2];
+                // sensorArray = m_shooterSub.RS232Listen(); ////////////////////////TODO Remove
+                m_ledSub.init();
+                m_climbSub.init();
+                m_drivetrainSub.init();
+                m_intakeSub.init();
+                m_shooterSub.init();
+                m_visionSub.init();
+
+                if(DriverStation.getAlliance().isPresent()) {
+                        if(DriverStation.getAlliance().get() == Alliance.Red) {
+                                m_isRedAlliance = true;
+                        } else if(DriverStation.getAlliance().get() == Alliance.Blue) {
+                                m_isRedAlliance = false;
+                        }
+                }
+        }
 }
 
