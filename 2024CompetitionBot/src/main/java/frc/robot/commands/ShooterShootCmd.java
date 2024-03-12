010d694c0c6cc7701328c4353018744bee3789ad
4917-Stronghold <software@4917.ca>
2024-03-12 16:03:21 -0400

    Update ShooterShootCmd.java

--- 2024CompetitionBot/src/main/java/frc/robot/commands/ShooterShootCmd.java
+++ 2024CompetitionBot/src/main/java/frc/robot/commands/ShooterShootCmd.java
@@ -4,51 +4,78 @@
 
 package frc.robot.commands;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.logging.Logger;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.ArduinoSub;
+import frc.robot.subsystems.FeederSub;
+import frc.robot.subsystems.FlywheelSub;
+import frc.robot.subsystems.LedSub;
 import frc.robot.subsystems.ShooterSub;
+import frc.robot.subsystems.LedSub.LedColour;
+import frc.robot.subsystems.LedSub.LedZones;
 
-public class ShooterPivotCmd extends Command {
-  private static Logger m_logger = Logger.getLogger(ShooterPivotCmd.class.getName());
+public class ShooterShootCmd extends Command {
+  private static Logger m_logger = Logger.getLogger(ShooterShootCmd.class.getName());
 
+  private final FlywheelSub m_flywheelSub;
+  private final FeederSub m_feederSub;
+  private final ArduinoSub m_arduinoSub;
   private final ShooterSub m_shooterSub;
-  private final double m_targetPivotPosition;
+  private final LedSub m_ledSub;
+  //private final ShooterFlywheelCmd m_shooterFlywheelCmd;
+  private Instant start;
 
-  /** Creates a new PivotCmd. */
-  public ShooterPivotCmd(double targetPivotPosition, ShooterSub shooterSub) {
-    m_targetPivotPosition = targetPivotPosition;
+  /** Creates a new ShooterShootCmd. */
+
+  public ShooterShootCmd(FlywheelSub flywheelSub, FeederSub feederSub, ArduinoSub arduinoSub, ShooterSub shooterSub,
+      LedSub ledSub) {
+    m_flywheelSub = flywheelSub;
+    m_feederSub = feederSub;
+    m_arduinoSub = arduinoSub;
     m_shooterSub = shooterSub;
+    m_ledSub = ledSub;
 
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(shooterSub);
+    addRequirements(flywheelSub, feederSub, shooterSub, ledSub);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_logger.fine("ShooterPivotCmd - Init");
-    m_shooterSub.setTargetAngle(m_targetPivotPosition);
-    m_shooterSub.runPivotControl(true);
+    m_logger.fine("ShooterShootCmd - Init");
+    start = Instant.now();
+    m_flywheelSub.enableFlywheel();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
 
+
+    // Flywheel needs to spin at full power prior to m_shooterSub.spinBothFeeders being executed.
+    // double driveOutput = m_FlyWheelPID.calculate(m_shooterSub.getFlywheelVelocity(), 4200); //10 is a target velocity we don't know what it is
+    m_feederSub.spinBothFeeders(Constants.Shooter.kNoteLowerIntakePower,
+        Constants.Shooter.kNoteUpperIntakePower);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_logger.fine("ShooterPivotCmd - End" + (interrupted ? " (interrupted)" : ""));
+    m_logger.fine("ShooterShootCmd - End" + (interrupted ? " (interrupted)" : ""));
+    m_feederSub.spinBothFeeders(0, 0);
+    m_flywheelSub.disableFlywheel();
+    m_ledSub.setZoneColour(LedZones.ALL, LedColour.GREEN);
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    boolean hitLimit = (m_shooterSub.getPivotPower() < 0.0) ? m_shooterSub.isPivotAtReverseLimit()
-        : m_shooterSub.isPivotAtForwardLimit(); // Emergency case to stop command
-
-    return m_shooterSub.isAtPivotAngle() || hitLimit;
+    if(m_arduinoSub.isSensorTripped(Constants.Shooter.kNoteSensorFwFar)) {
+      start = Instant.now();
+    }
+    Instant end = Instant.now();
+    Duration timeElapsed = Duration.between(start, end);
+    return timeElapsed.toSeconds() > 0.5;
   }
 }
