a557f169387d9202df1b2f10ddff4d6fea58f434
CarsonT0192 <car_jets@icloud.com>
2024-09-23 19:27:33 -0400

    Commented VisionSub.

--- 2024CompetitionBot/src/main/java/frc/robot/subsystems/VisionSub.java
+++ 2024CompetitionBot/src/main/java/frc/robot/subsystems/VisionSub.java
@@ -21,6 +21,18 @@ import frc.robot.RobotContainer;
 public class VisionSub extends SubsystemBase {
   private static Logger m_logger = Logger.getLogger(VisionSub.class.getName());
 
+  //Gets the limelight network table values (list of labels associated with values)
+  /*
+   * Important network table values:
+   * tv: 1 If valid target exists. 0 if no valid targets exist
+   * tx: Horizontal offset from crosshair to target (In degrees)
+   * ty: Vertical offset from crosshair to target (In degrees)
+   * ta: Target area, how much of the feed is covered by the limelight
+   * tid: Id of the primary apriltag currently in view
+   * botpose_targetspace: 3D transform from the primary in view apriltag
+   * getpipe: Active camera pipeline index (0,9)
+   */
+  //Go to https://docs.limelightvision.io/docs/docs-limelight/apis/complete-networktables-api for full list of network table values
   private final NetworkTable m_limelight;
   private final NetworkTable m_limelightDriver;
   private final ShuffleboardTab m_shuffleboardTab = Shuffleboard.getTab("Vision");
@@ -44,6 +56,7 @@ public class VisionSub extends SubsystemBase {
 
   /** Creates a new VisionSub. */
   public VisionSub() {
+    //Gets the network table
     m_limelight = NetworkTableInstance.getDefault().getTable("limelight");
     m_limelightDriver = NetworkTableInstance.getDefault().getTable("limelight-driver");
     m_tnx = m_limelightDriver.getEntry("tx");
@@ -57,6 +70,7 @@ public class VisionSub extends SubsystemBase {
     m_getpipe = m_limelight.getEntry("getpipe");
     m_pipeline = m_limelight.getEntry("pipeline");
 
+    //Adds values to the shuffleboard
     m_shuffleboardtx = m_shuffleboardTab.add("tx", 0).getEntry();
     m_shuffleboardty = m_shuffleboardTab.add("ty", 0).getEntry();
 
@@ -65,28 +79,34 @@ public class VisionSub extends SubsystemBase {
   }
 
   public void init() {
+    //Tells the log that teh sub is being intitiated
     m_logger.info("Initializing VisionSub");
+    //Sets the apriltag pipeline
     setPipeline(1); // Apriltag vision
   }
 
   @Override
   public void periodic() {
 
+    //The shuffleboard print isn't disabled, update the shuffleboard
     if(!RobotContainer.disableShuffleboardPrint) {
       // This method will be called once per scheduler run
       updateShuffleBoard();
     }
   }
 
+  //Method to get the apriltag heights from constants since not all have the same heeght, 
   public double getDistance(int id) { // In meters
-    if(id < 1 || id > 16) // Got bad apriltag
+    if(id < 1 || id > 16) // Got bad apriltag, since the ID values used are between 1 and 16 (inclusive)
       return 0.0;
     double height = Constants.Vision.kApriltagHeights[id + 1] + Constants.Vision.kApriltagOffset;
+    //Calculates the distacne from the apriltag to the robot using the calcDistacne mehtod
     double distance = calcDistance(height);
 
     return distance;
   }
 
+  //Updates shuffleboard with apriltag values
   public void updateShuffleBoard() {
     // Ref: https://docs.limelightvision.io/docs/docs-limelight/apis/json-dump-specification#apriltagfiducial-results
     // NOTE: (from doc) :  Takes up to 2.5ms on RoboRIO 1.0.
@@ -100,26 +120,36 @@ public class VisionSub extends SubsystemBase {
   }
 
 
+  //Trig explanation: the tangent of an angle equals opposite/adjacent (opposite is the side not forming the angle, adjacent is the other side
+  //which isn't the hypotenuse). If we call the angle θ, then tan(θ)=opposite/adjacent. If we know θ and the opposite (apriltag height), the we can re-arrange
+  //to adjacent=opposite/tan(θ) which is teh equation we use.
   private double calcDistance(double height) { // Basic trig
+    //Sets theta to the angle between the camera's crosshair and the april tag
     double theta = getSimpleVerticalAngle();
+    //Converts theta to radians
     theta = Math.toRadians(theta);
+    //Divides the height of the apriltag by the tangent of theta to get the distance from the april tag horizontally
     double distance = height / Math.tan(theta);
     return distance;
   }
 
+  //Method to create a pose3D from the botpose_target network table value
   public Pose3d getTarget3D() {
+    //Creates an array using the position values (0:2 are x,y,z, and 3:5 are yaw, roll, pitch. Not in those specific orders)
     double pos[] = m_botpose_target.getDoubleArray(new double[6]);
     Translation3d position = new Translation3d(pos[0], pos[1], pos[2]);
     Rotation3d rotation = new Rotation3d(pos[3], pos[4], pos[5]);
     return new Pose3d(position, rotation);
   }
 
+  //Gets only yaw, roll, and pitch using the same techniques as the previous method
   public Translation3d getTargetRotation() {
     double pos[] = m_botpose_target.getDoubleArray(new double[6]);
     Translation3d rotation = new Translation3d(pos[3], pos[4], pos[5]);
     return rotation;
   }
 
+  //Methods to retun respective network table values
   public double getSimpleHorizontalAngle() {
     return m_tx.getDouble(0.0);
   }
