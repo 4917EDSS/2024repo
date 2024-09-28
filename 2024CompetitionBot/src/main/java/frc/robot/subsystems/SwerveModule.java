afeffcb0cc0ce1d11804084c52ecd08daf097495
CarsonT0192 <car_jets@icloud.com>
2024-09-28 19:42:08 -0400

    Created a kraken compatible branch, unsure if it will work in its current state.

--- 2024CompetitionBot/src/main/java/frc/robot/subsystems/SwerveModule.java
+++ 2024CompetitionBot/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -11,10 +11,10 @@ import java.util.logging.Logger;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
+// import com.revrobotics.CANSparkBase.IdleMode;
+// import com.revrobotics.CANSparkLowLevel.MotorType;
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -30,9 +30,9 @@ public class SwerveModule extends SubsystemBase {
   private static Logger m_logger = Logger.getLogger(SwerveModule.class.getName());
 
   // Motors and Encoders
-  private final CANSparkMax m_driveMotor;
+  private final TalonFX m_driveMotor;
   public final TalonFX m_steeringMotor;
-  private final RelativeEncoder m_driveEncoder;
+  private final CANcoder m_driveEncoder;
   private final CANcoder m_steeringEncoder;
   private final double m_turningEncoderOffset;
 
@@ -63,14 +63,14 @@ public class SwerveModule extends SubsystemBase {
     // Initialize motors and sensors
 
     //Creates drive and steering motors/encoders
-    m_driveMotor = new CANSparkMax(driveMotorID, MotorType.kBrushless);
+    m_driveMotor = new TalonFX(driveMotorID);
     m_steeringMotor = new TalonFX(steeringMotorID);
-    m_driveEncoder = m_driveMotor.getEncoder();
+    m_driveEncoder = new CANcoder(driveMotorID);
     m_steeringEncoder = new CANcoder(steeringEncoderID);
 
     // Make it loop from -PI to PI
-    m_driveMotor.setIdleMode(IdleMode.kBrake);
-    m_driveMotor.setSmartCurrentLimit(40);
+    m_driveMotor.setNeutralMode(NeutralModeValue.Brake);
+    //m_driveMotor.setSmartCurrentLimit(40);
     m_steeringMotor.setNeutralMode(NeutralModeValue.Brake);
     m_steeringPID.enableContinuousInput(-Math.PI, Math.PI);
 
@@ -91,9 +91,10 @@ public class SwerveModule extends SubsystemBase {
     kDriveDistanceFactor = (Math.PI * Constants.ModuleConstants.kWheelBaseDiameter) / (GearRatio);
     kDriveVelocityFactor = kDriveDistanceFactor / 60.0;
 
+    //Not compatable with talonFX
     //Sets the conversion factor
-    m_driveEncoder.setVelocityConversionFactor(kDriveVelocityFactor);
-    m_driveEncoder.setPositionConversionFactor(kDriveDistanceFactor);
+    //m_driveEncoder.setVelocityConversionFactor(kDriveVelocityFactor);
+    //m_driveEncoder.setPositionConversionFactor(kDriveDistanceFactor);
 
     init();
   }
@@ -162,15 +163,16 @@ public class SwerveModule extends SubsystemBase {
     return position;
   }
 
+  //Division by kDriveVelocityFactor, and kDriveDistance factor might not be needed/correct
   //Gets the swerve module state (velocity and rotation)
   public SwerveModuleState getState() { // Swerve states are what Kinematics uses for calculations
-    return new SwerveModuleState(m_driveEncoder.getVelocity(),
+    return new SwerveModuleState(m_driveEncoder.getVelocity().getValueAsDouble() / kDriveVelocityFactor,
         new Rotation2d(getTurningRotation()));
   }
 
   //Gets the swerve module position (position and rotation)
   public SwerveModulePosition getPosition() { // Get current SwerveModule position
-    return new SwerveModulePosition(m_driveEncoder.getPosition(),
+    return new SwerveModulePosition(m_driveEncoder.getPosition().getValueAsDouble() / kDriveDistanceFactor,
         new Rotation2d(getTurningRotation()));
   }
 
@@ -198,7 +200,8 @@ public class SwerveModule extends SubsystemBase {
     betterState.speedMetersPerSecond *= betterState.angle.minus(steeringRotation).getCos();
 
     // Calculate power using velocity and PID
-    double driveOutput = m_drivePID.calculate(m_driveEncoder.getVelocity(), betterState.speedMetersPerSecond);
+    double driveOutput =
+        m_drivePID.calculate(m_driveEncoder.getVelocity().getValueAsDouble(), betterState.speedMetersPerSecond);
     double driveFeedforward = m_driveFeedforward.calculate(betterState.speedMetersPerSecond);
     // Calculate steering power using difference in angle
     double steeringOutput =
